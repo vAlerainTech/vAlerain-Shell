acd305a96ce55c80a27abd458c9db6b974176a38
vAlerian <151636139+WhiteFoxLinux@users.noreply.github.com>
2024-03-22 23:22:29 +0800

    All
    All

---
+++ Historical-source-code/1.0.0/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "all.h"
+
+using namespace std;
+void COLOR_PRINT(const char* s, int color)
+{
+ HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+ SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY | color);g
+ printf(s);
+ SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY | 7);
+}
+/*0 = 黑色 8 = 灰色
+1 = 蓝色 9 = 淡蓝色
+2 = 绿色 10 = 淡绿色
+3 = 浅绿色 11 = 淡浅绿色
+4 = 红色 12 = 淡红色
+5 = 紫色 13 = 淡紫色
+6 = 黄色 14 = 淡黄色
+7 = 白色 15 = 亮白色*/
+
+int main() {
+	
+	COLOR_PRINT("vAlerain-Shell",13); 
+	return 0;
+}
